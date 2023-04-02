360d58025cd039b9cee4fa1c98e34739f649a3a3
Jakub Piksa <kuba.piksa@wp.pl>
2023-04-02 19:49:05 +0200

    task 2 finally

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector(`ul#ingredients`);
+
+ingredients.forEach(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  ingredientsEl.append(listItem);
+});
