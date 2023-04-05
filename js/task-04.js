29cc23959a917d727777f2753fe374acb94e8dac
Jakub Piksa <kuba.piksa@wp.pl>
2023-04-05 22:36:10 +0200

    dua

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+//Utwórz zmienną counterValue w której będzie przechowywana aktualna wartość licznika i nadaj jej początkową wartość wartość 0.
+//Dodaj click listeners do przycisków, i przy ich pomocy zwiększaj i zmniejszaj wartość licznika.
+//Aktualizuj interfejs (widok HTML) nową wartością zmiennej counterValue po każdej jej zmianie.
+
+
+const counterValue = `#value`;
+
+
+
+
+
+
+
+
+console.log(counterValue);
