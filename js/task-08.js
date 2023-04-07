418058417332b75843398eb862f388ccfb0b7bad
Jakub Piksa <kuba.piksa@wp.pl>
2023-04-07 19:03:54 +0200

    7 8 9 wow kinda ql stuff i liked doing it

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const emailEl = document.querySelector('#email');
+const passwordEl = document.querySelector('#password');
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === '' || password.value === '') {
+    alert('Wszystkie pola powinny zostać wypełnione!');
+  } else console.log({ email: email.value, password: password.value });
+  event.currentTarget.reset();
+}
